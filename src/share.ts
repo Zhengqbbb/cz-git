9b53a46421b486a94a5162c8cf5d63f7cc5501f5
Zhengqbbb <1074059947@qq.com>
2022-02-16 09:57:00 +0800

    feat(config): update config type and default config

--- src/share.ts
+++ src/share.ts
@@ -27,6 +27,7 @@ export interface commitizenGitOptions {
     body?: string;
     breaking?: string;
     footer?: string;
+    footerPrefixsSelect?: string;
     confirmCommit?: string;
   };
 
@@ -36,9 +37,8 @@ export interface commitizenGitOptions {
   types?: typesOption[];
 
   /**
-   * @description: use emoji ？
+   * @description: use emoji ？| it will be use typesOption.emoji code
    * @default: false
-   * @see: TODO:
    */
   useEmoji?: boolean;
 
@@ -55,7 +55,7 @@ export interface commitizenGitOptions {
 
   /**
    * @description: Whether to not selectable skipping when appearing or customizing when selecting scopes
-   * @default false
+   * @default true
    */
   allowCustomScopes?: boolean;
 
@@ -67,17 +67,58 @@ export interface commitizenGitOptions {
 
   /**
    * @description: List of questions you want to skip
-   * @example: ['footer']
+   * @example: ['body']
    */
   skipQuestions?: string[];
 
   /**
    * @description: Provides a select issue prefix box in footer
-   * @default: [ {links: 'link to this issue', closed: 'The issue has been processed'} ]
+   * @default: issuePrefixs: [{ value: "closed", name: "ISSUES has been processed" }]
    */
-  issuePrefixs?: Option;
+  issuePrefixs?: Option[];
+
+  /**
+   * @description: prompt final determination whether to display the color
+   * @default: false
+   */
+  confirmNoColor?: boolean;
 }
 
+/* eslint-disable prettier/prettier */
+/* prettier-ignore */
 export const defaultConfig = Object.freeze({
-  types: [{ value: "feat", name: "feat:    A new feature", emoji: ":sparkles:" }]
+  messages: {
+    type: "Select the type of change that you're committing:",
+    scope: "Denote the SCOPE of this change (optional):",
+    customScope: "Denote the SCOPE of this change:",
+    subject: "Write a SHORT, IMPERATIVE tense description of the change:\n",
+    body: 'Provide a LONGER description of the change (optional). Use "|" to break new line:\n',
+    breaking: "List any BREAKING CHANGES (optional):\n",
+    footerPrefixsSelect: "Select the ISSUES type of changeList by this change (optional):\n",
+    footer: "List any ISSUES by this change. E.g.: #31, #34, #I972S:\n",
+    confirmCommit: "Are you sure you want to proceed with the commit above ?"
+  },
+  types: [
+    { value: "feat", name: "feat:    A new feature", emoji: ":sparkles:" },
+    { value: "fix", name: "fix:    A bug fix", emoji: ":bug:" },
+    { value: "docs", name: "docs:     Documentation only changes", emoji: ":memo:" },
+    { value: "style", name: "style:     Changes that do not affect the meaning of the code", emoji: ":lipstick:" },
+    { value: "refactor", name: "refactor: A code change that neither fixes a bug nor adds a feature", emoji: ":recycle:" },
+    { value: "perf", name: "perf:     A code change that improves performance", emoji: ":zap:" },
+    { value: "test", name: "test:     Adding missing tests or correcting existing tests", emoji: ":rotating_light:" },
+    { value: "build", name: "build:     Changes that affect the build system or external dependencies", emoji: ":building_construction:" },
+    { value: "ci", name: "ci:    Changes to our CI configuration files and scripts", emoji: ":green_heart:" },
+    { value: "chore", name: "chore:    Other changes that don't modify src or test files", emoji: ":hammer:" },
+    { value: "revert", name: "revert:    Reverts a previous commit", emoji: ":rewind:" }
+  ],
+  useEmoji: false,
+  scopes: [{ name: 'READEME' }, { name: 'theme' }, { name: 'module' }, { name: 'plugin' }],
+  allowCustomScopes: true,
+  allowBreakingChanges: ['feat', 'fix'],
+  skipQuestions: ['body'],
+  issuePrefixs: [
+    { value: "link", name: "processing to ISSUES" },
+    { value: "closed", name: "ISSUES has been processed" }
+  ],
+  confirmNoColor: false
 } as commitizenGitOptions);
