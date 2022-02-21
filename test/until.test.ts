d9d24ece4b0ab3faecc2000da0ff6a452ccee942
Zhengqbbb <1074059947@qq.com>
2022-02-21 11:39:42 +0800

    test(cz-git): add getMaxSubjectLength test

--- test/until.test.ts
+++ test/until.test.ts
@@ -1,29 +1,73 @@
-import { getMaxLength } from "../lib/until";
-import { Rule } from "../lib/until";
+import { getMaxSubjectLength } from "../lib/until";
 
 /**
- * @description: getMaxLength Test
+ * @description: getMaxSubjectLength Test
  */
-describe("getMaxLength()", () => {
-  test("use commitlint rule get subject max length", () => {
-    const clConfig = {
-      rules: {
-        "subject-max-length": [2, "always", 80]
-      }
-    };
-    expect(getMaxLength(clConfig.rules["subject-max-length"] as unknown as Rule)).toEqual(80);
+describe("getMaxSubjectLength", () => {
+  const answer = {
+    type: "feat",
+    scope: "cz-git"
+  };
+
+  test("underfined options subject rule", () => {
+    expect(getMaxSubjectLength(undefined, undefined, {})).toEqual(Infinity);
+    expect(getMaxSubjectLength(answer.type, undefined, {})).toEqual(Infinity);
+    expect(getMaxSubjectLength(answer.type, answer.scope, {})).toEqual(Infinity);
   });
 
-  test("underfined commitlint subject rule", () => {
-    const clConfig = {
-      rules: {
-        "subject-max-length": undefined
-      }
-    };
-    expect(getMaxLength(clConfig.rules["subject-max-length"] as unknown as Rule)).toEqual(Infinity);
+  test("use commitlint rule get subject max length", () => {
+    const options = { maxSubjectLength: 100 };
+    expect(getMaxSubjectLength(answer.type, answer.scope, options)).toEqual(86);
+  });
+
+  test("when use both maxsubject rule and maxheader rule", () => {
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: 120,
+        maxSubjectLength: 100
+      })
+    ).toEqual(86);
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: 100,
+        maxSubjectLength: 120
+      })
+    ).toEqual(86);
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: Infinity,
+        maxSubjectLength: Infinity
+      })
+    ).toEqual(Infinity);
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: Infinity,
+        maxSubjectLength: 100
+      })
+    ).toEqual(86);
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: 100,
+        maxSubjectLength: Infinity
+      })
+    ).toEqual(86);
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: 100,
+        maxSubjectLength: 0
+      })
+    ).toEqual(0);
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: 0,
+        maxSubjectLength: 100
+      })
+    ).toEqual(0);
+    expect(
+      getMaxSubjectLength(answer.type, answer.scope, {
+        maxHeaderLength: 100,
+        maxSubjectLength: 100
+      })
+    ).toEqual(86);
   });
 });
-
-// TODO: getMinLength
-
-// TODO: getEmojiStrLength
