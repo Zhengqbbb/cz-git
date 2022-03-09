66479d6f3a3dcb3976543ee3c29d720e5bf8a87f
Zhengqbbb <1074059947@qq.com>
2022-03-09 22:03:22 +0800

    test(cz-git): add question scope test

--- packages/cz-git/__tests__/buildCommit.test.ts
+++ packages/cz-git/__tests__/buildCommit.test.ts
@@ -9,6 +9,7 @@ describe("buildCommit()", () => {
     scope: "app",
     subject: "add a new feature"
   };
+
   test("subject with default subject standard separator", () => {
     const options = {};
     expect(buildCommit(answers, options)).toEqual("feat(app): add a new feature");
--- packages/cz-git/__tests__/generateQuestions.test.ts
+++ packages/cz-git/__tests__/generateQuestions.test.ts
@@ -12,7 +12,9 @@ describe("generateQuestions()", () => {
   });
 
   const mockedCz = {
-    Separator: {}
+    Separator: function () {
+      return this;
+    }
   };
   const fn = () => "";
   const getQuestion = (index: number) => {
@@ -30,17 +32,33 @@ describe("generateQuestions()", () => {
 
   test("test questions be returned", () => {
     options = {
-      types: [{ value: "feat", name: "feat: this is a feature" }]
+      types: [{ value: "feat", name: "feat: this is a feature" }],
+      scopes: ["cz-git"]
     };
     // test question 1 - type
     expect(getQuestion(1)?.name).toEqual("type");
     expect(getQuestion(1)?.type).toEqual("autocomplete");
-    const mockTypesSourceFn = getQuestion(1)?.source || fn;
+    let mockTypesSourceFn = getQuestion(1)?.source || fn;
     expect(mockTypesSourceFn({}, "f")).toEqual([
       {
         value: "feat",
         name: "feat: this is a feature"
       }
     ]);
+
+    // test question 2 - scope
+    expect(getQuestion(2)?.name).toEqual("scope");
+    expect(getQuestion(2)?.type).toEqual("autocomplete");
+    mockTypesSourceFn = getQuestion(2)?.source || fn;
+    expect(mockTypesSourceFn({}, "")).toEqual([
+      { name: "empty", value: false },
+      { name: "custom", value: "___CUSTOM___" },
+      {},
+      { name: "cz-git", value: "cz-git" }
+    ]);
+    expect(mockTypesSourceFn({}, "cz")).toEqual([{ name: "cz-git", value: "cz-git" }]);
+    expect(mockTypesSourceFn({}, "em")).toEqual([{ name: "empty", value: false }]);
+    expect(mockTypesSourceFn({}, "cu")).toEqual([{ name: "custom", value: "___CUSTOM___" }]);
+    expect(mockTypesSourceFn({}, "aaa")).toEqual([]);
   });
 });
