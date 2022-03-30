3b9173ceefaba5896b94be3388733de344448f15
Zhengqbbb <1074059947@qq.com>
2022-03-30 20:43:12 +0800

    test(cz-git): add test file for breaklineNumber
    link #6

--- packages/cz-git/__tests__/buildCommit.test.ts
+++ packages/cz-git/__tests__/buildCommit.test.ts
@@ -30,4 +30,23 @@ describe("buildCommit()", () => {
     };
     expect(buildCommit(answers, options)).toEqual("feat(app): add a new feature");
   });
+
+  test("body breaking line with breaklineNumber", () => {
+    const options = {
+      types: [{ value: "feat", name: "feat:     A new feature" }],
+      breaklineNumber: 20
+    };
+    const answers = {
+      type: "feat",
+      scope: "app",
+      subject: "add a new feature",
+      body: "test breaklineNumber test breaklineNumber"
+    };
+    expect(buildCommit(answers, options)).toEqual(
+      `feat(app): add a new feature
+
+test breaklineNumber
+test breaklineNumber`
+    );
+  });
 });
