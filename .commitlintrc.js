ee57bd0bbaba118e89daa61f75ed67dcf6cd56fd
Zhengqbbb <1074059947@qq.com>
2022-04-01 15:47:54 +0800

    chore(cz-git): update local config to show customIssuePrefixsAlign use

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,12 +2,12 @@ const fs = require('fs')
 const path = require('path')
 const { execSync } = require('child_process');
 
-// @tip: git branch name = feature/33   =>    auto get defaultIssues = #33
+// @description: git branch name = feature/33 => auto get defaultIssues = #33
 const issue = execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
   .trim()
   .split("/")[1]
-// @tip: monorepo dynamic get name
+// @description: monorepo dynamic get name
 const packages = fs.readdirSync(path.resolve(__dirname, 'packages/@cz-git'));
 
 /** @type {import('cz-git').UserConfig} */
@@ -24,6 +24,7 @@ module.exports = {
       { value: "link",   name: "link:     Work in processing to ISSUES" },
       { value: "closed", name: "closed:   ISSUES has been processed" }
     ],
-    defaultIssues: () => !issue ? "" : `#${issue}`
+    customIssuePrefixsAlign: !issue ? "top" : "bottom",
+    defaultIssues: !issue ? "" : `#${issue}`
   }
 };
