52697972d3685ca384148de74356a58f188ddc8d
Zhengqbbb <1074059947@qq.com>
2022-04-28 12:20:20 +0800

    perf: :zap: update vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,8 +4,8 @@ import { resolve } from "path";
 export default defineConfig({
   test: {
     root: resolve(__dirname),
-    include: ["**/__tests__/*.test.ts"],
     globals: true,
-    watchIgnore: ["**/node_modules/**", "**/dist/**", "**/lib/**"]
+    include: ["**/__tests__/*.test.ts"],
+    watchIgnore: ["**/node_modules/**", "**/dist/**", "**/lib/**", "**/docs/**"]
   }
 });
