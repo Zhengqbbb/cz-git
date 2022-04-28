e3fcdda7473dac6af32958f68c4ccea66bb3c72f
Zhengqbbb <1074059947@qq.com>
2022-04-28 12:43:52 +0800

    build: :package: use tsup build tool minify project modules

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,11 +1,13 @@
 import { defineConfig } from "tsup";
 
-export default defineConfig({
-  entry: ["./dist/index.js"],
-  outDir: "./lib",
-  tsconfig: "./tsconfig.build.json",
-  minify: true,
-  sourcemap: true,
-  dts: true,
-  splitting: false
+export default defineConfig((options) => {
+  return {
+    minify: !options.watch,
+    entry: !options.watch ? ["./dist/index.js"] : ["./src/index.js"],
+    outDir: "./lib",
+    tsconfig: "./tsconfig.build.json",
+    sourcemap: true,
+    dts: true,
+    splitting: false
+  };
 });
