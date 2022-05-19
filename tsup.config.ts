1dc4d6f755741e70838437998102340cd0090072
Zhengqbbb <1074059947@qq.com>
2022-05-19 10:58:38 +0800

    build: :package: update tsup bundle build cz-git

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,5 +1,11 @@
 import { defineConfig } from "tsup";
 
+/**
+ * NOTE: build types for `src/index.ts` only
+ * otherwise `Options` will not be exported by `tsup`, not sure how this happens
+ * probbably a bug in rollup-plugin-dts
+ */
+
 export default defineConfig((options) => {
   return {
     minify: false,
