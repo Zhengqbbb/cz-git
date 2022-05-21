a1b794ad5364a9cf2e5ab7abf5b1924f55532f6c
Zhengqbbb <1074059947@qq.com>
2022-05-22 00:18:59 +0800

    test(cz-git): update gen questions testcase

--- packages/cz-git/__tests__/generateQuestions.test.ts
+++ packages/cz-git/__tests__/generateQuestions.test.ts
@@ -1,73 +1,124 @@
-import { test, expect, describe, beforeEach } from "vitest";
+import { test, expect, describe, vitest, beforeEach } from "vitest";
 import { generateQuestions } from "../src/generator";
+import { CommitizenGitOptions } from "../src/shared/types/options";
 
 /**
  * @description: generateQuestions Test
  */
 
 describe("generateQuestions()", () => {
-  let options: any;
+  let options: CommitizenGitOptions;
 
   beforeEach(() => {
-    options = {};
+    options = null;
   });
 
   const mockedCz = {
-    Separator: function () {
-      return this;
-    }
+    Separator: vitest.fn()
   };
-  const fn = () => "";
-  const getQuestion = (index: number) => {
-    const question = generateQuestions(options, mockedCz);
-    if (question) {
-      return question[index - 1];
-    } else {
-      return undefined;
-    }
-  };
+  const getQuestion = (index: number) => generateQuestions(options, mockedCz)[index - 1];
 
   test("error config should be return false and print log", () => {
     expect(generateQuestions({}, undefined)).toBe(false);
   });
 
-  test("test questions should be returned", () => {
-    options = {
-      types: [{ value: "feat", name: "feat: this is a feature" }],
-      scopes: ["cz-git"]
-    };
-    // test question 1 - type
-    expect(getQuestion(1)?.name).toEqual("type");
-    expect(getQuestion(1)?.type).toEqual("search-list");
-    let mockTypesSourceFn = getQuestion(1)?.source || fn;
-    expect(mockTypesSourceFn({}, "f")).toEqual([
-      {
-        value: "feat",
-        name: "feat: this is a feature",
-        index: 0,
-        score: 1
-      }
-    ]);
+  describe("questions should be returned", () => {
+    describe("question 1 - type", () => {
+      beforeEach(() => {
+        options = {
+          types: [{ value: "feat", name: "feat: this is a feature" }],
+          scopes: ["cz-git"]
+        };
+      });
+      test("inquirer params should be right", () => {
+        expect(getQuestion(1).name).toEqual("type");
+        expect(getQuestion(1).type).toEqual("search-list");
+        expect(getQuestion(1).themeColorCode).toEqual(undefined);
+        options = {
+          themeColorCode: "38;5;043",
+          ...options
+        };
+        expect(getQuestion(1).themeColorCode).toEqual("38;5;043");
+      });
 
-    // test question 2 - scope
-    expect(getQuestion(2)?.name).toEqual("scope");
-    expect(getQuestion(2)?.type).toEqual("search-list");
-    mockTypesSourceFn = getQuestion(2)?.source || fn;
-    expect(mockTypesSourceFn({}, "")).toEqual([
-      { name: "empty", value: false },
-      { name: "custom", value: "___CUSTOM___" },
-      {},
-      { name: "cz-git", value: "cz-git" }
-    ]);
-    expect(mockTypesSourceFn({}, "cz")).toEqual([
-      { name: "cz-git", value: "cz-git", index: 3, score: 4 }
-    ]);
-    expect(mockTypesSourceFn({}, "em")).toEqual([
-      { name: "empty", value: false, index: 0, score: 4 }
-    ]);
-    expect(mockTypesSourceFn({}, "cu")).toEqual([
-      { name: "custom", value: "___CUSTOM___", index: 1, score: 4 }
-    ]);
-    expect(mockTypesSourceFn({}, "aaa")).toEqual([]);
+      test("source should be normal search work", () => {
+        const mockTypesSourceFn = vitest.fn(getQuestion(1).source);
+        expect(mockTypesSourceFn({}, "f")).toEqual([
+          {
+            value: "feat",
+            name: "feat: this is a feature",
+            index: 0,
+            score: 1
+          }
+        ]);
+      });
+    });
+
+    describe("question 2 - list scope", () => {
+      test("inquirer params should be right", () => {
+        options = {
+          types: [{ value: "feat", name: "feat: this is a feature" }],
+          scopes: ["cz-git"]
+        };
+        expect(getQuestion(2).name).toEqual("scope");
+        expect(getQuestion(2).type).toEqual("search-list");
+        expect(getQuestion(2).themeColorCode).toEqual(undefined);
+        options = {
+          themeColorCode: "38;5;043",
+          ...options
+        };
+        expect(getQuestion(2).themeColorCode).toEqual("38;5;043");
+      });
+
+      test("source should be normal search work", () => {
+        options = {
+          types: [{ value: "feat", name: "feat: this is a feature" }],
+          scopes: ["cz-git"]
+        };
+        const mockTypesSourceFn = vitest.fn(getQuestion(2).source);
+        expect(mockTypesSourceFn({}, "")).toEqual([
+          { name: "empty", value: false },
+          { name: "custom", value: "___CUSTOM___" },
+          {},
+          { name: "cz-git", value: "cz-git" }
+        ]);
+        expect(mockTypesSourceFn({}, "cz")).toEqual([
+          { name: "cz-git", value: "cz-git", index: 3, score: 4 }
+        ]);
+        expect(mockTypesSourceFn({}, "em")).toEqual([
+          { name: "empty", value: false, index: 0, score: 4 }
+        ]);
+        expect(mockTypesSourceFn({}, "cu")).toEqual([
+          { name: "custom", value: "___CUSTOM___", index: 1, score: 4 }
+        ]);
+        expect(mockTypesSourceFn({}, "aaa")).toEqual([]);
+        options = {
+          allowEmptyScopes: false,
+          ...options
+        };
+        const mockValidateFn = vitest.fn(getQuestion(2).validate);
+        expect(mockValidateFn("cz-git")).toBe(true);
+        expect(mockValidateFn("")).toMatch(/[ERROR]/);
+      });
+
+      test("only one item scope should be skip", () => {
+        options = {
+          types: [{ value: "feat", name: "feat: this is a feature" }],
+          scopes: ["cz-git"],
+          allowCustomScopes: true,
+          allowEmptyScopes: true
+        };
+        let mockWhenFn = vitest.fn(getQuestion(2).when);
+        expect(mockWhenFn({ types: "feat" })).toBe(true);
+        options = {
+          types: [{ value: "feat", name: "feat: this is a feature" }],
+          scopes: ["cz-git"],
+          allowCustomScopes: false,
+          allowEmptyScopes: false
+        };
+        mockWhenFn = vitest.fn(getQuestion(2).when);
+        expect(mockWhenFn({ types: "feat" })).toBe(false);
+      });
+    });
   });
 });
