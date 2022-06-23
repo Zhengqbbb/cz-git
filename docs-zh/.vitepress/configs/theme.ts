e332b21f38ec06d27b85ca1b126a8cfe9cc3af9a
Zhengqbbb <1074059947@qq.com>
2022-06-24 04:22:46 +0800

    docs(site): update homepage logo

--- docs-zh/.vitepress/configs/theme.ts
+++ docs-zh/.vitepress/configs/theme.ts
@@ -8,7 +8,7 @@ import { github, site } from "../meta";
  * @see https://vitepress.vuejs.org/config/theme-configs.html
  */
 export const themeConfig: DefaultTheme.Config = {
-  logo: "/images/logo.png",
+  logo: "https://user-images.githubusercontent.com/40693636/154064210-964aeaa0-d9dc-4cea-9e52-2ffc3789611b.png",
   socialLinks: [{ icon: "github", link: github }],
   editLink: {
     pattern: `${github}/edit/main/docs-zh/:path`,
