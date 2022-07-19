2d6cf26055f815ec88bb1feb7ad17980b06471a7
Zhengqbbb <1074059947@qq.com>
2022-07-20 03:32:30 +0800

    chore: replace `eslint-config` use antfu eslint config

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -1,39 +1,39 @@
-const { execSync } = require("child_process");
-const fg = require("fast-glob");
+const { execSync } = require('child_process')
+const fg = require('fast-glob')
 
 // @description: git branch name = feature/cli_33 => auto get defaultIssues = #33
-const issue = execSync("git rev-parse --abbrev-ref HEAD")
+const issue = execSync('git rev-parse --abbrev-ref HEAD')
   .toString()
   .trim()
-  .split("_")[1];
+  .split('_')[1]
 
 // @description: monorepo dynamic get name
-const packages = fg.sync('*', { cwd: "packages/@cz-git", onlyDirectories: true });
+const packages = fg.sync('*', { cwd: 'packages/@cz-git', onlyDirectories: true })
 
 /** @type {import('cz-git').UserConfig} */
 module.exports = {
-  extends: ["@commitlint/config-conventional"],
+  extends: ['@commitlint/config-conventional'],
   rules: {
-    "scope-enum": [2, "always", ["cz-git", "site", "cli", ...packages]],
-    "subject-min-length": [2, "always", 2],
-    "subject-empty": [2, "never"]
+    'scope-enum': [2, 'always', ['cz-git', 'site', 'cli', ...packages]],
+    'subject-min-length': [2, 'always', 2],
+    'subject-empty': [2, 'never'],
   },
   prompt: {
     // @see: https://github.com/Zhengqbbb/cz-git#options
-    alias: { 
-      b: "chore: bump dependencies",
-      c: "chore: update config files",
-      f: "docs: fix typos",
-      ":": "docs: update README",
-      "table:data": "chore: :hammer: update project using table data",
-      "table:docs": "docs: update project using table",
+    alias: {
+      'b': 'chore: bump dependencies',
+      'c': 'chore: update config files',
+      'f': 'docs: fix typos',
+      ':': 'docs: update README',
+      'table:data': 'chore: :hammer: update project using table data',
+      'table:docs': 'docs: update project using table',
     },
-    themeColorCode: "38;5;043",
+    themeColorCode: '38;5;043',
     issuePrefixs: [
-      { value: "link",   name: "link:     Work in processing to ISSUES" },
-      { value: "closed", name: "closed:   ISSUES has been processed" }
+      { value: 'link', name: 'link:     Work in processing to ISSUES' },
+      { value: 'closed', name: 'closed:   ISSUES has been processed' },
     ],
-    customIssuePrefixsAlign: !issue ? "top" : "bottom",
-    defaultIssues: !issue ? "" : `#${issue}`
-  }
-};
+    customIssuePrefixsAlign: !issue ? 'top' : 'bottom',
+    defaultIssues: !issue ? '' : `#${issue}`,
+  },
+}
--- scripts/projects.ts
+++ scripts/projects.ts
@@ -3,82 +3,85 @@
  * @Use `pnpm docs:update`
  * @Author: Qbenben
  * @LastEditors: Qbenben
- * @LastEditTime: 2022-07-12 00:46:26
+ * @LastEditTime: 2022-07-20 03:22:03
  */
-import ora from "ora";
-import yaml from "js-yaml";
-import fs from "fs";
-import { resolve } from "pathe";
 
-const CZ_GIT_READMES = ["../packages/cz-git/README.md", "../README.md"];
+import fs from 'fs'
+import ora from 'ora'
+import yaml from 'js-yaml'
+import { resolve } from 'pathe'
+
+const CZ_GIT_READMES = ['../packages/cz-git/README.md', '../README.md']
 const CZ_git_DOCS = [
-  "../docs/guide/introduction.md",
-  "../docs/zh/guide/introduction.md",
-  "../docs-zh/guide/introduction.md"
-];
-const CZG_READMES = ["../packages/cli/README.md"];
-const CZG_DOCS = ["../docs/cli/index.md", "../docs/zh/cli/index.md", "../docs-zh/cli/index.md"];
+  '../docs/guide/introduction.md',
+  '../docs/zh/guide/introduction.md',
+  '../docs-zh/guide/introduction.md',
+]
+const CZG_READMES = ['../packages/cli/README.md']
+const CZG_DOCS = ['../docs/cli/index.md', '../docs/zh/cli/index.md', '../docs-zh/cli/index.md']
 
-type Source = {
-  name: string;
-  link: string;
-  icon: string;
-};
+interface Source {
+  name: string
+  link: string
+  icon: string
+}
 
-type Data = {
-  czg: Source[];
-  "cz-git": Source[];
-};
+interface Data {
+  czg: Source[]
+  'cz-git': Source[]
+}
 
-const genProjectUseCzGite = (data: Data, key: "czg" | "cz-git", isBreak: boolean) => {
-  const source = data[key];
-  let result = "<table>\n  <tr>";
+const genProjectUseCzGite = (data: Data, key: 'czg' | 'cz-git', isBreak: boolean) => {
+  const source = data[key]
+  let result = '<table>\n  <tr>'
   source.forEach((item, index) => {
     if ((index !== 0 && (index + 1) % 4 === 0) || index === source.length - 1) {
       result += `\n    <td align="center" width="200px">
       <a target="_blank" href="${item.link}">
-        <img src="${item.icon}" alt="${item.name} logo" width="40">${isBreak ? "<br>" : ""}
+        <img src="${item.icon}" alt="${item.name} logo" width="40">${isBreak ? '<br>' : ''}
         <sub>${item.name}</sub>
       </a>
     </td>
-  </tr>${index === source.length - 1 ? "" : "\n  <tr>"}`;
-    } else {
+  </tr>${index === source.length - 1 ? '' : '\n  <tr>'}`
+    }
+    else {
       result += `\n    <td align="center" width="200px">
       <a target="_blank" href="${item.link}">
-        <img src="${item.icon}" alt="${item.name} logo" width="40">${isBreak ? "<br>" : ""}
+        <img src="${item.icon}" alt="${item.name} logo" width="40">${isBreak ? '<br>' : ''}
         <sub>${item.name}</sub>
       </a>
-    </td>`;
+    </td>`
     }
-  });
-  return (result += "\n</table>");
-};
+  })
+  return (result += '\n</table>')
+}
 
-const genTableByPaths = (paths: string[], data: any, key: "czg" | "cz-git", isBreak: boolean) => {
+const genTableByPaths = (paths: string[], data: any, key: 'czg' | 'cz-git', isBreak: boolean) => {
   paths.forEach((p) => {
-    let readme = fs.readFileSync(resolve(__dirname, p), "utf8");
-    if (readme) {
-      readme = readme.replace(/<table>(.|\n)*?<\/table>/g, genProjectUseCzGite(data, key, isBreak));
-    }
-    fs.writeFileSync(resolve(__dirname, p), readme);
-  });
-};
+    let readme = fs.readFileSync(resolve(__dirname, p), 'utf8')
+    if (readme)
+      readme = readme.replace(/<table>(.|\n)*?<\/table>/g, genProjectUseCzGite(data, key, isBreak))
+
+    fs.writeFileSync(resolve(__dirname, p), readme)
+  })
+}
 
 // Main
-const spinner = ora("\u001b[38;5;043mUpdate Project using cz-git Table...\u001b[0m 📦").start();
+const spinner = ora('\u001B[38;5;043mUpdate Project using cz-git Table...\u001B[0m 📦').start()
 try {
-  const start = Date.now();
-  const data = yaml.load(fs.readFileSync(resolve(__dirname, "./projects.yml"), "utf8"));
+  const start = Date.now()
+  const data = yaml.load(fs.readFileSync(resolve(__dirname, './projects.yml'), 'utf8'))
 
-  genTableByPaths(CZ_GIT_READMES, data, "cz-git", true);
-  genTableByPaths(CZ_git_DOCS, data, "cz-git", false);
-  genTableByPaths(CZG_READMES, data, "czg", true);
-  genTableByPaths(CZG_DOCS, data, "czg", false);
+  genTableByPaths(CZ_GIT_READMES, data, 'cz-git', true)
+  genTableByPaths(CZ_git_DOCS, data, 'cz-git', false)
+  genTableByPaths(CZG_READMES, data, 'czg', true)
+  genTableByPaths(CZG_DOCS, data, 'czg', false)
   spinner.succeed(
-    `\u001b[38;5;043mSuccessfully updated table! ${((Date.now() - start) / 1000).toFixed(
-      2
-    )}s\u001b[0m ✨`
-  );
-} catch (e) {
-  spinner.fail("post-build enchance pages failed: " + e);
+    `\u001B[38;5;043mSuccessfully updated table! ${((Date.now() - start) / 1000).toFixed(
+      2,
+    )}s\u001B[0m ✨`,
+  )
+}
+catch (e) {
+  spinner.fail(`post-build enchance pages failed: ${e}`)
 }
