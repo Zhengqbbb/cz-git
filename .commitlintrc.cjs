7b8470a05a0f214537e132683dffd7508b2d70e4
Zhengqbbb <1074059947@qq.com>
2022-07-20 02:42:20 +0800

    chore: update `alias` config

--- .commitlintrc.cjs
+++ .commitlintrc.cjs
@@ -21,10 +21,10 @@ module.exports = {
   prompt: {
     // @see: https://github.com/Zhengqbbb/cz-git#options
     alias: { 
-      r: "docs: update README",
       b: "chore: bump dependencies",
       c: "chore: update config files",
-      ":": "docs: fix typos",
+      f: "docs: fix typos",
+      ":": "docs: update README",
       "table:data": "chore: :hammer: update project using table data",
       "table:docs": "docs: update project using table",
     },
