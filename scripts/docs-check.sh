5db791a606af90cbb1af17108006be2c78d3ddd5
Zhengqbbb <1074059947@qq.com>
2022-08-22 18:48:10 +0800

    chore(site): use netlify replace ghpage
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -11,6 +11,9 @@ git diff --quiet "$CACHED_COMMIT_REF" "$COMMIT_REF" \
     "scripts/docs-check.sh"
 
 _diff_exit_code=$?
-[ "$_diff_exit_code" -eq 0 ] || \
-	printf "\033[36m%s\033[1;31m%s\033[0m\n" "diff exit code: " "$_diff_exit_code"
+if [ "$_diff_exit_code" -eq 0 ]; then
+	printf "\033[32m%s\033[1;36m%s\033[0m\n" "No need deploy page" "diff exit code: $_diff_exit_code"
+else
+	printf "\033[1;36m%s\033[1;31m%s\033[0m\n" "Need deploy page. diff exit code: " "$_diff_exit_code"
+fi
 exit "$_diff_exit_code"
