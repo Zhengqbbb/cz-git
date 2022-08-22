04f516489c1567b50e00e941b5c2e3e9d6a1b058
Zhengqbbb <1074059947@qq.com>
2022-08-22 10:53:34 +0800

    docs(site): use netlify replace ghpage
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -1,3 +1,3 @@
 # shellcheck shell=bash disable=
 git diff --quiet "$CACHED_COMMIT_REF" "$COMMIT_REF" \
-    docs docs-zh package.json pnpm-lock.yaml netlify.toml scripts/docs-check.sh
+    "docs/" "docs-zh/" "package.json" "pnpm-lock.yaml" "netlify.toml" "scripts/docs-check.sh"
