a516a1368b4ad6db279516ad2907ef7f6b9c4b6d
Zhengqbbb <1074059947@qq.com>
2022-08-22 18:52:04 +0800

    chore(site): use netlify replace ghpage
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- scripts/docs-check.sh
+++ scripts/docs-check.sh
@@ -1,6 +1,6 @@
 # shellcheck shell=bash disable=
-printf "\033[36m%s\033[1;37m%s\033[0m\n" "prev commit: " 	 	"$CACHED_COMMIT_REF"
-printf "\033[36m%s\033[1;37m%s\033[0m\n" "current commit: " "$CACHED_COMMIT_REF"
+printf "\033[36m%s\033[1;37m%s\033[0m\n" "prev commit: " 	 "$CACHED_COMMIT_REF"
+printf "\033[36m%s\033[1;37m%s\033[0m\n" "current commit: "  "$COMMIT_REF"
 
 git diff --quiet "$CACHED_COMMIT_REF" "$COMMIT_REF" \
     "docs/"           \
@@ -12,8 +12,8 @@ git diff --quiet "$CACHED_COMMIT_REF" "$COMMIT_REF" \
 
 _diff_exit_code=$?
 if [ "$_diff_exit_code" -eq 0 ]; then
-	printf "\033[32m%s\033[1;36m%s\033[0m\n" "No need deploy page" "diff exit code: $_diff_exit_code"
+	printf "\033[1;32m%s\033[36m%s\033[0m\n" "No need deploy page." "diff exit code: $_diff_exit_code"
 else
-	printf "\033[1;36m%s\033[1;31m%s\033[0m\n" "Need deploy page. diff exit code: " "$_diff_exit_code"
+	printf "\033[1;36m%s\033[31m%s\033[0m\n" "Need deploy page. diff exit code: "   "$_diff_exit_code"
 fi
 exit "$_diff_exit_code"
