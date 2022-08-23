b71bed013d905a795a4c56cc293c16ff71f096b1
Zhengqbbb <1074059947@qq.com>
2022-08-24 03:13:33 +0800

    docs: fix typos
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- docs-zh/.vitepress/configs/theme.ts
+++ docs-zh/.vitepress/configs/theme.ts
@@ -9,7 +9,7 @@ import * as sidebar from './sidebar'
  */
 export const themeConfig: DefaultTheme.Config = {
   logo: {
-    src: '/images/logo.png',
+    src: 'https://user-images.githubusercontent.com/40693636/154064210-964aeaa0-d9dc-4cea-9e52-2ffc3789611b.png',
     alt: 'cz-git-logo',
   },
   socialLinks: [
