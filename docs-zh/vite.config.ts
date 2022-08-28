888469e2a782c434b7a64816e51082128fffec60
Zhengqbbb <1074059947@qq.com>
2022-08-28 17:45:55 +0800

    chore: bump dependencies
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- docs-zh/vite.config.ts
+++ docs-zh/vite.config.ts
@@ -1,12 +1,6 @@
 import { defineConfig } from 'vite'
-import { pwaPlugin, unocssPlugin } from '../docs/.vitepress/build'
+import { pwaPlugin, unocssPlugin } from './.vitepress/build'
 
 export default defineConfig({
   plugins: [unocssPlugin, pwaPlugin],
-  server: {
-    fs: {
-      // Allow serving files from one level up to the project root
-      allow: ['..'],
-    },
-  },
 })
