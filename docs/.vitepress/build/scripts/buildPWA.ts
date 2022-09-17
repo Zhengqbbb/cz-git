57b704870e65bc47da62623fd4a0a313dcccdd67
Zhengqbbb <1074059947@qq.com>
2022-09-17 16:27:28 +0800

    docs(site): use `buildEnd hook` to optimize page
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- docs/.vitepress/build/scripts/buildPWA.ts
+++ docs/.vitepress/build/scripts/buildPWA.ts
@@ -6,8 +6,8 @@ export const rebuildPWA = async () => {
   const config = await resolveConfig({}, 'build', 'production')
   const pwaPlugin = config.plugins.find(i => i.name === 'vite-plugin-pwa')?.api
   if (pwaPlugin && pwaPlugin.generateSW && !pwaPlugin.disabled) {
-    await pwaPlugin.generateSW()
     await optimizePages()
+    await pwaPlugin.generateSW()
     console.log('\x1B[32m✓\x1B[0m regenerate PWA ServiceWorker... \x1B[90m[buildEnd]\x1B[0m')
   }
 }
