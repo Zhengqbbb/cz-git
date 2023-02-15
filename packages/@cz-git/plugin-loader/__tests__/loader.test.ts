687e2a481d5f90b8f70630e768cfca21613de028
Zhengqbbb <1074059947@qq.com>
2023-02-15 20:54:39 +0800

    test: test config loader to set openAIToken to empty string
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- packages/@cz-git/plugin-loader/__tests__/loader.test.ts
+++ packages/@cz-git/plugin-loader/__tests__/loader.test.ts
@@ -1,5 +1,6 @@
 import { afterEach, describe, expect, test } from 'vitest'
 import type tmp from 'tmp'
+import type { UserOptions } from '../src'
 import { configLoader } from '../src'
 import { useBootstrap } from './utils'
 
@@ -10,45 +11,51 @@ describe('config loader', () => {
       mockDir.removeCallback()
   })
 
+  const loaderSpyFn = async (opt: UserOptions) => {
+    const res = await configLoader(opt)
+    res.prompt.openAIToken = ''
+    return res
+  }
+
   test('basic packages.json config should be loaded', async () => {
     mockDir = await useBootstrap('./fixtures/1-basic')
-    const config = await configLoader({ cwd: mockDir.name })
-    expect(config).toEqual({ prompt: { path: 'node_modules/cz-git', useEmoji: true } })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
+    expect(config).toEqual({ prompt: { path: 'node_modules/cz-git', useEmoji: true, openAIToken: '' } })
   }, 1000)
 
   test('basic commitlint js config should be loaded', async () => {
     mockDir = await useBootstrap('./fixtures/1-basic-commitlint-js')
-    const config = await configLoader({ cwd: mockDir.name })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
     expect(config).toEqual({
       rules: { 'scope-enum': [2, 'always', ['cz-git']] },
-      prompt: { path: 'node_modules/cz-git', useEmoji: true },
+      prompt: { path: 'node_modules/cz-git', useEmoji: true, openAIToken: '' },
     })
   }, 1000)
 
   test('basic commitlint json config should be loaded', async () => {
     mockDir = await useBootstrap('./fixtures/1-basic-commitlint-json')
-    const config = await configLoader({ cwd: mockDir.name })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
     expect(config).toEqual({
       rules: { 'scope-enum': [2, 'always', ['cz-git']] },
-      prompt: { path: 'node_modules/cz-git', useEmoji: true },
+      prompt: { path: 'node_modules/cz-git', useEmoji: true, openAIToken: '' },
     })
   }, 1000)
 
   test('basic commitizen js config should be loaded', async () => {
     mockDir = await useBootstrap('./fixtures/1-basic-cz-js')
-    const config = await configLoader({ cwd: mockDir.name })
-    expect(config).toEqual({ prompt: { useEmoji: true } })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
+    expect(config).toEqual({ prompt: { useEmoji: true, openAIToken: '' } })
   }, 1000)
 
   test('basic commitizen json config should be loaded', async () => {
     mockDir = await useBootstrap('./fixtures/1-basic-cz-json')
-    const config = await configLoader({ cwd: mockDir.name })
-    expect(config).toEqual({ prompt: { path: 'node_modules/cz-git', useEmoji: true } })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
+    expect(config).toEqual({ prompt: { path: 'node_modules/cz-git', useEmoji: true, openAIToken: '' } })
   }, 1000)
 
   test('commitizen extends config should be loaded', async () => {
     mockDir = await useBootstrap('./fixtures/2-extend-commitlint')
-    const config = await configLoader({ cwd: mockDir.name })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
     expect(config).toEqual({
       rules: {
         'scope-enum': [2, 'always', ['cz-git']],
@@ -58,13 +65,14 @@ describe('config loader', () => {
       prompt: {
         useEmoji: true,
         customIssuePrefixAlign: 'top',
+        openAIToken: '',
       },
     })
   }, 1000)
 
   test('config function should be call', async () => {
     mockDir = await useBootstrap('./fixtures/3-config-function')
-    const config = await configLoader({ cwd: mockDir.name })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
     expect(config).toEqual({
       rules: {
         'scope-enum': [2, 'always', ['cz-git']],
@@ -75,26 +83,29 @@ describe('config loader', () => {
         useEmoji: true,
         customIssuePrefixAlign: 'top',
         themeColorCode: '38;5;043',
+        openAIToken: '',
       },
     })
   }, 1000)
 
   test('custom config should be loaded', async () => {
     mockDir = await useBootstrap('./fixtures/4-custom-config')
-    const config = await configLoader({ cwd: mockDir.name })
+    const config = await loaderSpyFn({ cwd: mockDir.name })
     expect(config).toEqual({
       prompt: {
         useEmoji: true,
+        openAIToken: '',
       },
     })
   }, 1000)
 
   test('specify the configuration path', async () => {
     mockDir = await useBootstrap('./fixtures/5-specify-custom-config')
-    const config = await configLoader({ cwd: mockDir.name, configPath: './config/cz.custom.js' })
+    const config = await loaderSpyFn({ cwd: mockDir.name, configPath: './config/cz.custom.js' })
     expect(config).toEqual({
       prompt: {
         useEmoji: true,
+        openAIToken: '',
       },
     })
   }, 1000)
