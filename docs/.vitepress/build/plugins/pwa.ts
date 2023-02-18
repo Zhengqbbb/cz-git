c5a8d456080208e3f9606491105aceef397536a5
Zhengqbbb <1074059947@qq.com>
2023-02-19 02:32:14 +0800

    chore: update config files

--- docs/.vitepress/build/plugins/pwa.ts
+++ docs/.vitepress/build/plugins/pwa.ts
@@ -46,7 +46,7 @@ export const pwa: Partial<VitePWAOptions> = {
   },
   workbox: {
     navigateFallbackDenylist: [/^\/new$/],
-    globPatterns: ['**/*.{js,css,webp,png,svg,gif,ico,woff2}'],
+    globPatterns: ['**/*.{css,webp,png,svg,gif,ico,woff2}'],
     navigateFallback: null,
     runtimeCaching: [
       {
