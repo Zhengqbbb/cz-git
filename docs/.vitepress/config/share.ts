d461b2b57e6c7cb71270aeee194132bf7c5907f0
Zhengqbbb <1074059947@qq.com>
2023-03-09 11:18:29 +0800

    docs(site): update page analytics
    Co-authored-by: Zhengqbbb <1074059947@qq.com>

--- docs/.vitepress/config/share.ts
+++ docs/.vitepress/config/share.ts
@@ -70,6 +70,6 @@ export const shareConfig = defineConfig({
     ['link', { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.googleapis.com' }],
     ['link', { rel: 'preconnect', crossorigin: 'anonymous', href: 'https://fonts.gstatic.com' }],
     // page analytics
-    ['script', { 'async': '', 'defer': '', 'data-website-id': `${process.env.UMAIMI_ID || ''}`, 'src': `${process.env.UMAIMI_ENDPOINT || ''}` }],
+    ['script', { 'async': '', 'defer': '', 'data-website-id': `${process.env.UMAMI_ID || ''}`, 'src': `${process.env.UMAMI_ENDPOINT || ''}` }],
   ],
 })
