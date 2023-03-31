982d928a165b980fcf857c8fbf95790b4442ecab
Q.Ben Zheng <40693636+Zhengqbbb@users.noreply.github.com>
2023-04-01 00:30:41 +0800

    refactor: improve building and tsup usage (#116)
    Co-authored-by: Jrson <jrson83@users.noreply.github.com>

--- packages/cli/src/shared/types/types.d.ts
+++ packages/cli/src/shared/types/types.d.ts
@@ -1,17 +1,17 @@
-declare module "dedent" {
+declare module 'dedent' {
   /**
    * ES6 string tag that strips indentation from multi-line strings.
    * @see: https://github.com/dmnd/dedent
    */
-  export default function dedent(str: string): string;
+  export default function dedent(str: string): string
 }
 
-declare module "cachedir" {
+declare module 'cachedir' {
   /**
    * Get a directory for your caching needs
    * @see: https://github.com/LinusU/node-cachedir
    * @param {string} name
    * @return {string} cacheDir
    */
-  export default function cachedir(name: string): string;
+  export default function cachedir(name: string): string
 }
